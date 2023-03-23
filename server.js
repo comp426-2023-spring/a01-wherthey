6fcaf3a650c6dfa235cb81af1ef6e20eaadfb10f
Nick Wherthey <nwherthey@unc.edu>
2023-03-22 20:14:31 -0400

    got a 0, now have output in terminal but can only type numbers in command line

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const argv = require("minimist")(process.argv.slice(2));
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 
-const port = argv['port'] || 3000;
+const port = argv.port || 3000;
+
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
@@ -20,7 +21,10 @@ const port = argv['port'] || 3000;
 // The stuff that should be inside this function is all below.
 
 fs.readFile('./public/index.html', 'utf8', (err, data) => {
-    if (err) throw err;
+    if (err) {
+        console.log(err);
+        return err;
+    }
     console.log(data);
 
 
