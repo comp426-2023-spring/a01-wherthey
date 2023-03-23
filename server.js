7ebeb63d683d0266b7b578f886374498051f6ec1
Nick Wherthey <nwherthey@unc.edu>
2023-03-22 20:41:24 -0400

    got a 0, now have output in terminal but can only type numbers in command line

--- server.js
+++ server.js
@@ -21,40 +21,21 @@ const port = argv.port || 3000;
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
 
-fs.readFile('./public/index.html', 'utf-8', (err, data) => {
+const data = fs.readFile('./public/index.html', 'utf8', (err, data) => {
     if (err) {
         console.error(err);
         return;
     }
+    console.log(data);
+});
 
-    const server = http.createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(data);
-    })
-
-    server.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
-      });
-})
-
-// If there is an error, put it on the console error and return. 
-// Do not be nice about exiting.
-
-// Define a const `server` as an arrow function using http.createServer. 
-// Use the documentation for the node.js http module. 
-// The function should have three responses: 
-// 1. status code 200, 
-// 2. set a header with content type `text/html`, and 
-// 3. end with the data that you are reading in from ./public/index.html.
-
-
-// Start the `server` const listening on the port defined by argument in your `port` const. 
-// Put the exact message `Server listening on port ${port}` on the console log. 
-
-
-
-
-// That's it! You're all done!
-
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(data);
+  });
+  
+  server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
 
